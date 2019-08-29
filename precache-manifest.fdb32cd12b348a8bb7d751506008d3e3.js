6fc70866b2c00cf5d8484c0edefe0273575ff4ef
Shirasawa <764798966@qq.com>
2019-08-30 04:16:33 +0800

    Updates

---
+++ precache-manifest.fdb32cd12b348a8bb7d751506008d3e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3ba7e8e83fcfbcb95e7d2c9029cce7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e53eaaae8d3019dd0fa1",
+    "url": "/static/css/2.abe39327.chunk.css"
+  },
+  {
+    "revision": "e6c11a9252f650a7419b",
+    "url": "/static/css/main.70221edc.chunk.css"
+  },
+  {
+    "revision": "e53eaaae8d3019dd0fa1",
+    "url": "/static/js/2.7e97d300.chunk.js"
+  },
+  {
+    "revision": "0f62daf3e6601e9ca8be",
+    "url": "/static/js/3.e545a11f.chunk.js"
+  },
+  {
+    "revision": "e6c11a9252f650a7419b",
+    "url": "/static/js/main.1978a56a.chunk.js"
+  },
+  {
+    "revision": "03ce16116d50ac0c4d4a",
+    "url": "/static/js/runtime~main.74400556.js"
+  }
+]);
